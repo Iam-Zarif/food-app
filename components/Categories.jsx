b761982661d4998475b76b1d5c0fb47bb051b123
Iam-Zarif <zarifraja19@gmail.com>
2024-05-19 23:07:03 +0600

    menu

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { categoris } from "../constants/api";
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View className="mt-4">
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="overflow-visible"
+        contentContainerStyle={{
+          paddingHorizontal: 20,
+          paddingBottom: 20,
+        }}
+      >
+        {categoris.map((category, key) => {
+          return (
+            <View key={key} className="flex justify-center items-center mr-4">
+              <TouchableOpacity className="px-4 py-1.5 rounded-full shadow bg-gray-200">
+                <View className="flex-col items-center">
+                  <Image className="h-10 w-10" source={category.image}></Image>
+                  <Text className="text-xs">{category.name}</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
-  )
+  );
 }
--- constants/api.jsx
+++ constants/api.jsx
@@ -1,18 +1,38 @@
-export const itemsList = [
+export const categoris = [
   {
     id: 1,
-    name: "Item One",
-    image: require("./path/to/image1.jpg"),
+    name: "Cake",
+    image: require('../assets/images/categories/1.png'),
   },
   {
     id: 2,
-    name: "Item Two",
-    image: require("./path/to/image2.jpg"),
+    name: "Burger",
+    image: require('../assets/images/categories/2.png'),
   },
   {
     id: 3,
-    name: "Item Three",
-    image: require("./path/to/image3.jpg"),
+    name: "Pizza",
+    image: require('../assets/images/categories/3.png'),
+  },
+  {
+    id: 4,
+    name: "Honey",
+    image: require('../assets/images/categories/4.png'),
+  },
+  {
+    id: 5,
+    name: "Cake",
+    image: require('../assets/images/categories/5.png'),
+  },
+  {
+    id: 6,
+    name: "Drinks",
+    image: require('../assets/images/categories/6.png'),
+  },
+  {
+    id: 7,
+    name: "Fish",
+    image: require('../assets/images/categories/7.png'),
   },
 ];
 
